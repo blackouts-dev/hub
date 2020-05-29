53a56a7409f1a7855aba568ba3cc78b2d3148a2b
Jonah Snider <jonah@jonah.pw>
2020-05-29 16:48:29 -0700

    feat(presence): add validation of messages
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/presence/presence.service.ts
+++ src/presence/presence.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { transformAndValidate } from 'class-transformer-validator';
 import { Repository } from 'typeorm';
 import { Presence } from './presence.entity';
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
 export class PresenceService {
@@ -15,17 +16,26 @@ export class PresenceService {
     return this.presenceRepository.find();
   }
 
+  /**
+   * Executed when a presence is reported by the Discord bot.
+   * @param msg The message from the bot
+   */
   @RabbitSubscribe({
     exchange: 'presence',
     routingKey: 'bot.presence.update',
     queue: 'record-presence',
   })
-  public async storePresence(msg: { bot_id: string; online: boolean }): Promise<void> {
-    await this.presenceRepository.save({
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      bot_id: msg.bot_id,
-      online: msg.online,
-      when: new Date(),
-    });
+  public async onPresenceUpdate(msg: Record<string, unknown>): Promise<void> {
+    let presence: Presence;
+
+    try {
+      presence = await transformAndValidate(Presence, msg);
+    } catch (error) {
+      Logger.error('Invalid message from RabbitMQ', error);
+    }
+
+    Logger.debug(presence);
+
+    this.presenceRepository.insert(presence);
   }
 }
